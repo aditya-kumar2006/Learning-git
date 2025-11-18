e13631bc645faf9ae443c033485afc222afc2fca
aditya-kumar-patraan1 <pythonboot06@gmail.com>
2025-11-18 23:41:24 +0530

    changes in newOne

--- newone.py
+++ newone.py
@@ -1,1 +1,7 @@
 print("This is a new file for collaboration.")
+
+def collaboration_function():
+    return "This function is for collaboration purposes."
+
+if __name__ == "__main__":
+    print(collaboration_function())
