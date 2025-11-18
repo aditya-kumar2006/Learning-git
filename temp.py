8b20a9b12a4c47211661994cc4869f480e0d6088
aditya-kumar2006 <adityakrp2006@gmail.com>
2025-11-18 22:01:38 +0530

    changes in temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,27 @@
 print("this is a temp file")
+
+def temp_function():
+    return "This is a temporary function"
+
+if __name__ == "__main__":
+    print(temp_function())
+def temp_function():
+    return "This is a temporary function"
+if __name__ == "__main__":
+    print(temp_function())
+print("this is a temp file")
+def temp_function():
+    return "This is a temporary function"
+if __name__ == "__main__":
+    print(temp_function())
+print("this is a temp file")
+def temp_function():
+    return "This is a temporary function"
+if __name__ == "__main__":
+    print(temp_function())
+print("this is a temp file")
+
+
+
+for u in range(1000):
+    print("Hello word for i : ",u);
